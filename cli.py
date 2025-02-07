5afbeb39ef2ac1c442225d3691d7d718f55c26e3
Abdullahi <aadahiye@gmail.com>
2025-02-07 03:22:42 +0300

    method to add a new user

--- cli.py
+++ cli.py
@@ -1,3 +1,17 @@
 import click
 from database import session
 from models import User, Capsule, Message
+
+@click.group()
+def cli():
+    """Time Capsule CLI - Store and Retrieve Digital Time Capsules"""
+    pass
+
+@cli.command()
+@click.argument("name")
+def add_user(name):
+    """Add a new user"""
+    user = User(name=name)
+    session.add(user)
+    session.commit()
+    print(f"User '{name}' added successfully.")
