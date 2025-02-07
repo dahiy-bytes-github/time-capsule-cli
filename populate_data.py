f17498be9a15c882edf88b102fa30fabd1d49f0a
Abdullahi <aadahiye@gmail.com>
2025-02-07 03:37:43 +0300

    Added some users

--- populate_data.py
+++ populate_data.py
@@ -1,2 +1,22 @@
 from database import session
 from models import User, Capsule, Message
+
+# Adding some users
+users = [
+    User(name="Kwame Mensah"),
+    User(name="Zainab Bello"),
+    User(name="Thabo Mokoena"),
+    User(name="Amina Yusuf"),
+    User(name="Obafemi Adewale"),
+    User(name="Fatoumata Keita"),
+    User(name="Moses Chukwuma"),
+    User(name="Tariq Abubakar"),
+    User(name="Nia Okonkwo"),
+    User(name="Dayo Eze"),
+    User(name="Chinwe Mbatha"),
+    User(name="Bakari Juma"),
+    User(name="Adjoa Owusu"),
+]
+
+session.add_all(users)
+session.commit()
