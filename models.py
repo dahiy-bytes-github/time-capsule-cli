7a25e16f20ba25a9f298b6ce3708dad31a30270a
Abdullahi <aadahiye@gmail.com>
2025-02-07 03:10:02 +0300

    Generate encryption key

--- models.py
+++ models.py
@@ -3,3 +3,14 @@ from sqlalchemy.orm import relationship
 from cryptography.fernet import Fernet
 import os
 from database import Base, session
+
+# Generate encryption key 
+KEY_FILE = "secret.key"
+if not os.path.exists(KEY_FILE):
+    with open(KEY_FILE, "wb") as f:
+        f.write(Fernet.generate_key())
+
+with open(KEY_FILE, "rb") as f:
+    ENCRYPTION_KEY = f.read()
+
+cipher = Fernet(ENCRYPTION_KEY)
